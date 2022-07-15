25e65f150585545639faf389c4d1a473edb14ff5
John Kane <john@kanej.me>
2022-07-15 10:31:09 +0100

    test: add chain id test

--- examples/simple/ignition/MyModule.js
+++ examples/simple/ignition/MyModule.js
@@ -3,8 +3,6 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition");
 module.exports = buildModule("MyModule", (m) => {
   const foo = m.contract("Foo");
 
-
-  console.log("ChainId:", m.chainId)
   // m.call(foo, "inc", {
   //   args: [1]
   // })
