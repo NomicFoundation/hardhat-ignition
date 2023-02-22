aea1cb39f006ecd93d3ea8e06e4aa78ccf0aacc2
zoeyTM <morgansliman@gmail.com>
2023-02-22 01:27:49 -0500

    fix BigNumber in example

--- examples/sample/ignition/LockModule.js
+++ examples/sample/ignition/LockModule.js
@@ -5,7 +5,7 @@ const currentTimestampInSeconds = Math.round(new Date(2023, 01, 01) / 1000);
 const TEN_YEAR_IN_SECS = 10 * 365 * 24 * 60 * 60;
 const TEN_YEARS_IN_FUTURE = currentTimestampInSeconds + TEN_YEAR_IN_SECS;
 
-const ONE_GWEI = hre.ethers.utils.parseUnits("1", "gwei");
+const ONE_GWEI = hre.ethers.utils.parseUnits("1", "gwei").toString();
 
 module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getOptionalParam("unlockTime", TEN_YEARS_IN_FUTURE);
