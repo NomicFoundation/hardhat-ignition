732c90b4bf7eaa260f292ba0de94bb6cabb9f18b
zoeyTM <morgansliman@gmail.com>
2023-04-05 22:02:52 -0400

    add doc tags for info types

--- packages/core/src/types/info.ts
+++ packages/core/src/types/info.ts
@@ -1,14 +1,29 @@
+/**
+ * A data structure describing a deployed Module
+ *
+ * @internal
+ */
 export interface ModuleInfoData {
   moduleName: string;
   networks: NetworkInfoData[];
 }
 
+/**
+ * A data structure describing network info for a deployed Module
+ *
+ * @internal
+ */
 export interface NetworkInfoData {
   networkName: string;
   chainId: number;
   contracts: ContractInfoData[];
 }
 
+/**
+ * A data structure describing a deployed Contract
+ *
+ * @internal
+ */
 export interface ContractInfoData {
   contractName: string;
   status: string;
