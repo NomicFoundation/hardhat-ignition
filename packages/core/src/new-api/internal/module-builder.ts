ca53f69b5d74e8ccbe54eda9536978347ecdad9b
zoeyTM <morgansliman@gmail.com>
2023-05-21 23:27:01 -0400

    adding default any type

--- packages/core/src/new-api/internal/module-builder.ts
+++ packages/core/src/new-api/internal/module-builder.ts
@@ -368,7 +368,7 @@ export class IgnitionModuleBuilderImplementation<
     return future;
   }
 
-  public getParameter<ParamType extends SolidityParamType>(
+  public getParameter<ParamType extends SolidityParamType = any>(
     parameterName: string,
     defaultValue?: ParamType
   ): ParamType {
