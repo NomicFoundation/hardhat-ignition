e89d247a302aa34a2dc642fd0a08455577254ffd
John Kane <john@kanej.me>
2023-12-01 16:58:42 +0000

    refactor: revert unneeded optional parameter

--- packages/core/src/internal/views/find-deployed-contracts.ts
+++ packages/core/src/internal/views/find-deployed-contracts.ts
@@ -5,38 +5,27 @@ import {
   ContractAtExecutionState,
   DeploymentExecutionState,
   ExecutionSateType,
-  ExecutionState,
   ExecutionStatus,
 } from "../execution/types/execution-state";
 import { assertIgnitionInvariant } from "../utils/assertions";
 
-export function findDeployedContracts(
-  deploymentState: DeploymentState,
-  contractFilter: (
-    exState: ExecutionState
-  ) => exState is
-    | DeploymentExecutionState
-    | ContractAtExecutionState = _isExStateAContractState
-): {
+export function findDeployedContracts(deploymentState: DeploymentState): {
   [futureId: string]: DeployedContract;
 } {
   return Object.values(deploymentState.executionStates)
-    .filter(contractFilter)
+    .filter(
+      (
+        exState
+      ): exState is DeploymentExecutionState | ContractAtExecutionState =>
+        exState.type === ExecutionSateType.DEPLOYMENT_EXECUTION_STATE ||
+        exState.type === ExecutionSateType.CONTRACT_AT_EXECUTION_STATE
+    )
     .filter((des) => des.status === ExecutionStatus.SUCCESS)
     .map(_toDeployedContract)
     .reduce<{ [futureId: string]: DeployedContract }>((acc, contract) => {
       acc[contract.id] = contract;
       return acc;
     }, {});
-}
-
-function _isExStateAContractState(
-  exState: ExecutionState
-): exState is DeploymentExecutionState | ContractAtExecutionState {
-  return (
-    exState.type === ExecutionSateType.DEPLOYMENT_EXECUTION_STATE ||
-    exState.type === ExecutionSateType.CONTRACT_AT_EXECUTION_STATE
-  );
 }
 
 function _toDeployedContract(
