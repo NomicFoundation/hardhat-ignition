d7ebcba854437e3b7749c86fdd92face21adddf5
John Kane <john@kanej.me>
2023-12-15 16:25:19 +0000

    test: update test text

--- packages/core/test-integrations/new-api/ci/basic-success.ts
+++ packages/core/test-integrations/new-api/ci/basic-success.ts
@@ -3,13 +3,14 @@ import { assert } from "chai";
 import { useHardhatProject } from "../../helpers/hardhat-projects";
 
 // This test exists to ensure Ignition succeeds in a CI environment.
+// This is a test that the UI, runs even in constrained terminal environments.
 // It should always pass locally.
-describe("CI - basic success case", function () {
+describe("Running deployment in CI environment", function () {
   this.timeout(60000);
 
   useHardhatProject("ci-success");
 
-  it("should succeed in a CI environment", async function () {
+  it("should succeed with UI in a CI environment", async function () {
     await assert.isFulfilled(
       this.hre.run(
         { scope: "ignition", task: "deploy" },
