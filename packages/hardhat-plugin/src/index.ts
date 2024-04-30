fc90abfe89de8e5c5ec194cb6fa3e464aaa96bc3
Zoey <probablyzoey+github@gmail.com>
2024-04-30 05:27:44 -0400

    Integrating hardhat-ledger support (#731)
    Integrated the new `hardhat-ledger` support for controlling its UI display. Ignition now takes control of displaying the messages from the ledger (e.g. "Connect wallet").
    
    Fixes #720

--- packages/hardhat-plugin/src/index.ts
+++ packages/hardhat-plugin/src/index.ts
@@ -6,12 +6,12 @@ import {
   StatusResult,
 } from "@nomicfoundation/ignition-core";
 import {
+  ensureDir,
+  pathExists,
+  readFile,
   readdirSync,
   rm,
-  pathExists,
   writeJSON,
-  ensureDir,
-  readFile,
 } from "fs-extra";
 import { extendConfig, extendEnvironment, scope } from "hardhat/config";
 import { NomicLabsHardhatPluginError } from "hardhat/plugins";
@@ -257,6 +257,37 @@ ignitionScope
       const strategyConfig = hre.config.ignition.strategyConfig?.[strategyName];
 
       try {
+        try {
+          await hre.network.provider.send("hardhat_setLedgerOutputEnabled", [
+            false,
+          ]);
+
+          hre.network.provider.once(
+            "connection_start",
+            executionEventListener.ledgerConnectionStart
+          );
+          hre.network.provider.once(
+            "connection_success",
+            executionEventListener.ledgerConnectionSuccess
+          );
+          hre.network.provider.once(
+            "connection_failure",
+            executionEventListener.ledgerConnectionFailure
+          );
+          hre.network.provider.on(
+            "confirmation_start",
+            executionEventListener.ledgerConfirmationStart
+          );
+          hre.network.provider.on(
+            "confirmation_success",
+            executionEventListener.ledgerConfirmationSuccess
+          );
+          hre.network.provider.on(
+            "confirmation_failure",
+            executionEventListener.ledgerConfirmationFailure
+          );
+        } catch {}
+
         const result = await deploy({
           config: hre.config.ignition,
           provider: hre.network.provider,
@@ -272,6 +303,37 @@ ignitionScope
           maxFeePerGasLimit:
             hre.config.networks[hre.network.name]?.ignition.maxFeePerGasLimit,
         });
+
+        try {
+          await hre.network.provider.send("hardhat_setLedgerOutputEnabled", [
+            true,
+          ]);
+
+          hre.network.provider.off(
+            "connection_start",
+            executionEventListener.ledgerConnectionStart
+          );
+          hre.network.provider.off(
+            "connection_success",
+            executionEventListener.ledgerConnectionSuccess
+          );
+          hre.network.provider.off(
+            "connection_failure",
+            executionEventListener.ledgerConnectionFailure
+          );
+          hre.network.provider.off(
+            "confirmation_start",
+            executionEventListener.ledgerConfirmationStart
+          );
+          hre.network.provider.off(
+            "confirmation_success",
+            executionEventListener.ledgerConfirmationSuccess
+          );
+          hre.network.provider.off(
+            "confirmation_failure",
+            executionEventListener.ledgerConfirmationFailure
+          );
+        } catch {}
 
         if (result.type === "SUCCESSFUL_DEPLOYMENT" && verify) {
           console.log("");
